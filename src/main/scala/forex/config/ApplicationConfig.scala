07503c1a7ca2fd3ce458304acebf1e073bfacf1e
IanChien <g104029013@gmail.com>
2023-05-23 06:25:20 +0800

    feat: Add one frame client class

--- src/main/scala/forex/config/ApplicationConfig.scala
+++ src/main/scala/forex/config/ApplicationConfig.scala
@@ -4,10 +4,19 @@ import scala.concurrent.duration.FiniteDuration
 
 case class ApplicationConfig(
     http: HttpConfig,
+    oneFrameClient: OneFrameClientConfig
 )
 
 case class HttpConfig(
     host: String,
     port: Int,
     timeout: FiniteDuration
+)
+
+case class OneFrameClientConfig(
+    host: String,
+    port: Int,
+    token: String,
+    timeout: Int, // connection timeout(seconds)
+    updateFreq: Int // Update frequency(minutes)
 )
